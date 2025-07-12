6843948595a38ebcfb79c5840d35fa5b8d2f9b12
Nitin Gayke <gaykenitin975@gmail.com>
2025-07-12 15:00:08 +0530

    updated rate limit requests

--- Server/middlewares/rateLimiter.js
+++ Server/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 5,
   standardHeaders: true,
   legacyHeaders: false,
 
